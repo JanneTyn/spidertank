d4589593dbb1806cf0784052f8f1dc29fa5b7688
Janne <jannetyni98@gmail.com>
2024-05-02 11:56:55 +0300

    melko basicci liikkuminen ja kamera

---
+++ Assets/ThirdPersonCam.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.UIElements;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    public GameObject playerScript;
+    public CinemachineFreeLook camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var transposer = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineOrbitalTransposer>();
+        //float XRotation = transposer.m_XAxis.Value;
+        float XRotation = camera.m_XAxis.Value;
+        Debug.Log("rotation: " + XRotation);
+
+    }
+}
