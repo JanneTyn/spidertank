bcef63a5f5586f9b345000b1991a9b5bbdea7520
lauriasdf <lauri.hilden1@gmail.com>
2024-05-10 10:41:52 +0300

    spawner alku

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Spawner : MonoBehaviour
+{
+
+	public GameObject enemyPrefab;
+
+
+
+	public Transform[] spawnPoints;
+
+	public void SpawnRandom()
+	{
+		Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, enemyPrefab.transform.rotation);
+	}
+
+    private void Start()
+    {
+		StartCoroutine(spawning());
+			
+    }
+
+	IEnumerator spawning()
+    {
+		while (true)
+        {
+			SpawnRandom();
+			yield return new WaitForSeconds(0.6f);
+
+        }
+    }
+}
