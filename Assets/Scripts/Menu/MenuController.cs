777abb101dbd899114665694882e2da015004879
nikwunu <nikwunu@gmail.com>
2024-05-15 14:11:30 +0300

    menu controller wip

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuController : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject pauseMenu;
+    public GameObject UpgradeMenu;
+    PlayerLeveling Levelingscript;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }  
+    }
+    void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f; // Pause the game
+        pausemenu.SetActive(true); // Show the pause menu UI
+    }
+
+    void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f; // Unpause the game
+        pauseMenu.SetActive(false); // Hide the pause menu UI
+    }
+
+        public Upgrade()
+    {
+        isPaused = true;
+        Time.timeScale = 0f; // Pause the game
+        UpgradeMenu.SetActive(true); // Show the pause menu UI
+    }
+}
---
+++ Assets/Scripts/Menu/ResolutionDropdown.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+
+public class ResolutionDropdown : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    void Start()
+    {
+        // Get available resolutions
+        resolutions = Screen.resolutions;
+
+        // Clear existing options
+        resolutionDropdown.ClearOptions();
+
+        // Create a list of resolution strings
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            // Check if this resolution matches the current screen size
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        // Add options to the dropdown
+        resolutionDropdown.AddOptions(options);
+
+        // Set the default value to the current resolution
+        resolutionDropdown.value = currentResolutionIndex;
+
+        // Refresh the displayed value
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    // Call this method when the user selects a resolution from the dropdown
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
