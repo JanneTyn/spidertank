c98476e2ebb77139be86bc4e03780794f5bcb8b9
lauriasdf <lauri.hilden1@gmail.com>
2024-05-21 13:35:20 +0300

    spawnpoint (korvaa spawnerin) tarvii viel lisäilyä

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,49 +7,88 @@ class Spawner : MonoBehaviour
 {
 
 
-	public Transform[] m_SpawnPoints;
+
 	public GameObject m_EnemyPrefab;
 	public GameObject m_EnemyRangePrefab;
-	
-	public Wave wave;
+
+
 
 	Transform player;
 
-	int instanceNumber = 1;
+
 
 	public int aliveEnemyCount;
 	public int spawnRange;
 	public int waitTime;
 
-	public int enemyCount;
-
-	private int randomNumber;
+	public static int maxEnemies = 5;
+	public static int enemyCount = 0;
 	
 
+
+	private int randomNumber;
+
+
+	public float startspawndis;
+	public float currentdis;
+
+
+
+
+
 	void OnEnable()
-    {
-		
-	}
-    void Start()
-    {
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-		StartCoroutine("WaitAndSpawn");
-		//spawning();
+	{
+
 	}
 
-    void Update()
-    {
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+
+		//spawning();
+
+	}
+
+
+	void Update()
+	{
+
+		
+
+		currentdis = Vector3.Distance(player.transform.position, transform.position);
+
+		if (this.gameObject != null)
+		{
+			if (currentdis > startspawndis)
+			{
+				StopCoroutine("WaitAndSpawn");
+
+			}
+			else if (currentdis < startspawndis)
+			{
+
+				MobCheck();
+			}
+			
+		}
+
+
+
+	}
+
+	void MobCheck()
+	{
+
 		aliveEnemyCount = FindObjectsOfType<Enemy>().Length;
 		GameObject[] gameObjects;
 		gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
 
 		if (gameObjects.Length == 0)
 		{
-			
+
 			StartCoroutine("WaitAndSpawn");
 			//spawning();
 		}
-		
 	}
 
 	private Vector3 GenerateSpawnPos()
@@ -58,34 +97,43 @@ class Spawner : MonoBehaviour
 		float spawnPosX = Random.Range(-spawnRange, spawnRange);
 		float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
-		Vector3 spawnPos = player.transform.position + new Vector3(spawnPosX, 0.91f, spawnPosZ);
+		Vector3 spawnPos = transform.position + new Vector3(spawnPosX, 0.91f, spawnPosZ);
 
 		return spawnPos;
 
 
 	}
 
-	
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(transform.position, new Vector3(spawnRange, 0.0f, spawnRange));
+	}
+
+
 
 	private IEnumerator WaitAndSpawn()
 	{
-		
+		Debug.Log("started spawning");
 		yield return new WaitForSeconds(waitTime);
 		Spawn();
-		enemyCount += 3;
+		//enemyCount += 2;
+		
 
-		StopCoroutine("WaitAndSpawn");
+		
 	}
 
 	
-	 void Spawn()
+	void Spawn()
 	{
-	
-		for (int i = 0; i < enemyCount; i++)
+		int enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+
+		if (enemyCount < maxEnemies)
 		{
-		
+			enemyCount++;
 			randomNumber = Random.Range(0, 3);
-			
+
 			switch (randomNumber)
 			{
 				case 0: Instantiate(m_EnemyPrefab, GenerateSpawnPos(), m_EnemyPrefab.transform.rotation); ; break;
@@ -93,31 +141,28 @@ class Spawner : MonoBehaviour
 				case 1: Instantiate(m_EnemyRangePrefab, GenerateSpawnPos(), m_EnemyRangePrefab.transform.rotation); break;
 
 				case 2: Instantiate(m_EnemyPrefab, GenerateSpawnPos(), m_EnemyPrefab.transform.rotation); break;
+
+
 			}
-			
 		}
-		
-	}
-/*
-	void spawning()
-	{
-		
-
-		for (int i = 0; i < wave.enemyCount; i++)
+	/*	for (int i = 0; i < enemyCount; i++)
 		{
-			int randomNumber = Mathf.RoundToInt(Random.Range(0f, m_SpawnPoints.Length - 1));
 
-			GameObject currentEntity = Instantiate(m_EnemyPrefab, m_SpawnPoints[randomNumber].transform.position, Quaternion.identity);
+			randomNumber = Random.Range(0, 3);
 
-			
+			switch (randomNumber)
+			{
+				case 0: Instantiate(m_EnemyPrefab, GenerateSpawnPos(), m_EnemyPrefab.transform.rotation); ; break;
 
-			
+				case 1: Instantiate(m_EnemyRangePrefab, GenerateSpawnPos(), m_EnemyRangePrefab.transform.rotation); break;
 
-			currentEntity.name = wave.prefabName + instanceNumber;
-			instanceNumber++;
+				case 2: Instantiate(m_EnemyPrefab, GenerateSpawnPos(), m_EnemyPrefab.transform.rotation); break;
+
+
+			}
 
 		}
+	*/
 	}
-*/
 }
 
