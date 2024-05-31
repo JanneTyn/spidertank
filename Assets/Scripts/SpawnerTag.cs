fd58ee21df3f6f17a6eb5533a6d9dcc486490cb3
lauriasdf <lauri.hilden1@gmail.com>
2024-05-31 13:31:13 +0300

    spawn fix

--- Assets/Scripts/SpawnerTag.cs
+++ Assets/Scripts/SpawnerTag.cs
@@ -6,18 +6,19 @@ public class SpawnerTag : MonoBehaviour
 {
 	public float startspawndis;
 	public float currentdis;
+	public float tooclosedis;
 
 	Transform player;
 
 	// Start is called before the first frame update
 	void Start()
-    {
+	{
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		currentdis = Vector3.Distance(player.transform.position, transform.position);
 
 		if (this.gameObject != null)
@@ -32,6 +33,14 @@ public class SpawnerTag : MonoBehaviour
 
 				gameObject.tag = "Point";
 			}
+			if (currentdis <= tooclosedis)
+			{
+
+				gameObject.tag = "Untagged";
+
+			}
+
+
 
 		}
 	}
